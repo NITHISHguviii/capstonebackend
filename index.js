3dd1efca23cb07064205318ae24f0acc1927250d
NITHISHguviii <nithishkumarv0612@gmail.com>
2023-05-29 15:35:14 +0530

    port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config();
 const app = express();
 
 import { MongoClient } from "mongodb";
-const PORT = 4000;
+const PORT = process.env.PORT;
 //const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env.MONGO_URL;
 const client = new MongoClient(MONGO_URL);
